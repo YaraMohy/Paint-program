8bdc1a00d5f8f483a72a8e50a64f922e25b797e7
Ashrakat Rashwan <140539192+ashrakatrashwan@users.noreply.github.com>
2023-07-31 16:18:54 +0300

    Update PaintingApp.java

--- PaintingApp.java
+++ PaintingApp.java
@@ -125,3 +125,76 @@ public class PaintingApp extends Applet{
 		this.addMouseMotionListener(ml);
 		
 	}
+	public void paint(Graphics g){
+		int width = getSize().width;   
+     		int height = getSize().height;
+		g.setColor(Color.white);
+     		g.fillRect(0,0, width, height);
+		g.setColor(Color.black);
+      		g.drawRect(0, 0, width-1, height-1);
+		g.drawRect(1, 1, width-3, height-3);
+		g.drawRect(2, 2, width-5, height-5);
+		g.drawRect(3, 3, width-7, height-7);
+		cirB.setBackground(Color.gray);
+		cirB.setForeground(Color.white);
+		rectB.setBackground(Color.gray);
+		rectB.setForeground(Color.white);
+		lineB.setBackground(Color.gray);
+		lineB.setForeground(Color.white);
+		rb.setBackground(Color.red);
+		rb.setForeground(Color.white);
+		yb.setBackground(Color.yellow);
+		yb.setForeground(Color.white);
+		blackb.setBackground(Color.black);
+		blackb.setForeground(Color.white);
+		gb.setBackground(Color.green);
+		gb.setForeground(Color.white);
+		bb.setBackground(Color.blue);
+		bb.setForeground(Color.white);
+		cirB.setSize(width/9,height/10);
+		cirB.setLocation(5,5);
+		rectB.setSize(width/9,height/10);
+		rectB.setLocation((width/9)+5,5);
+		lineB.setSize(width/9,height/10);
+		lineB.setLocation(2*(width/9)+5,5);
+		penB.setSize(width/9,height/10);
+		penB.setLocation(3*(width/9)+5,5);
+		fillB.setSize(width/9,height/10);
+		fillB.setLocation(4*(width/9)+5,5);
+		eraserB.setSize(width/9,height/10);
+		eraserB.setLocation(5*(width/9)+5,5);
+		rb.setSize((width/9),height/20);
+		rb.setLocation(6*(width/9)+5,5);
+		yb.setSize((width/9),height/20);
+		yb.setLocation(7*(width/9),5);
+		gb.setSize((width/9),height/20);
+		gb.setLocation(6*(width/9)+5,(height/20)+5);
+		bb.setSize((width/9),(height/20)+5);
+		bb.setLocation(7*(width/9),height/20);
+		blackb.setSize((width/9),height/10);
+		blackb.setLocation(8*(width/9)-2,5);
+		float[] dash = {10,10,10};
+		Graphics2D g2d = (Graphics2D) g.create();
+			for (j=0; j< shapesVector.size(); j++){
+				g2d.setColor(shapesVector.get(j).getColor());	
+				switch(shapesVector.get(j).getShapeName()){
+					case 'l':
+						g2d.drawLine(shapesVector.get(j).getX1(), shapesVector.get(j).getY1(), shapesVector.get(j).getX2(), shapesVector.get(j).getY2());
+						break;
+					case 'r':
+						g2d.drawRect(shapesVector.get(j).getX1(), shapesVector.get(j).getY1(), shapesVector.get(j).getX2(), shapesVector.get(j).getY2());	
+						if(shapesVector.get(j).isFilled())
+							g2d.fillRect(shapesVector.get(j).getX1(), shapesVector.get(j).getY1(), shapesVector.get(j).getX2(), shapesVector.get(j).getY2());
+							break;
+					case 'c':
+						g2d.drawOval(shapesVector.get(j).getX1(), shapesVector.get(j).getY1(), shapesVector.get(j).getX2(), shapesVector.get(j).getY2());	
+						if(shapesVector.get(j).isFilled())
+							g2d.fillOval(shapesVector.get(j).getX1(), shapesVector.get(j).getY1(), shapesVector.get(j).getX2(), shapesVector.get(j).getY2());	
+							break;
+					case 'e':
+						g2d.fillRect(shapesVector.get(j).getX1(), shapesVector.get(j).getY1(), eraserWidth, eraserHeight);		
+				}
+			
+			}	
+		g.setColor(currentColor);
+	}
