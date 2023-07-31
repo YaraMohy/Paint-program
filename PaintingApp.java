1a4dfd8fab37e59a5ea1b1f15d343ad9e6a42a99
Nada77Sayed <140098880+Nada77Sayed@users.noreply.github.com>
2023-07-31 16:42:12 +0300

    Update PaintingApp.java

--- PaintingApp.java
+++ PaintingApp.java
@@ -253,4 +253,65 @@ class MotionListener implements MouseMotionListener{
 					break;
 			}
 		}
+		class GeoShape{
+		protected int dim1;
+		protected int x1; 
+		protected int y1;
+		protected int x2; 
+		protected int y2;
+		private char whoAmI;
+		protected boolean filledPressed;
+		protected Color color;
+		public GeoShape(){
+		}
+		public GeoShape(int x1, int y1, int x2, int y2, char whoAmI, boolean filledPressed){
+			this.x1=x1;
+			this.y1=y1;
+			this.x2=x2;
+			this.y2=y2;
+			this.whoAmI=whoAmI;
+			this.filledPressed=filledPressed;
+			if(whoAmI == 'e')
+				this.color =getBackground();
+			else
+				this.color= currentColor;
+		}
+		public GeoShape(int x1, int y1, int x2, int y2, char whoAmI){
+			this.x1=x1;
+			this.y1=y1;
+			this.x2=x2;
+			this.y2=y2;
+			this.whoAmI=whoAmI;
+			this.color= currentColor;
+		}
+		boolean isFilled(){
+			return filledPressed;
+		}
+		void setShape(char p){
+			whoAmI=p;
+		}
+		char getShape(){
+			return whoAmI;
+		}
+		void setX1(int x1){
+			x1=x1;
+		}
+		void setY1(int y1){
+			y1=y1;
+		}
+		public void setX2(int x2){
+			x2=x2;
+		}
+		public void setY2(int Y2){
+			y2=y2;
+		}
+		public void setDim(int d){
+			if (d <1)
+				System.out.print("SORRY NO NEGATIVE NUMBERS!");
+			else
+				dim1=d;
+		}
+		
+	}
+	
 	}
